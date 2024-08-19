9a6424e46373d159ca0fda7b52afe94c07ccd802
Your Name <ruben.radu@stud.ubbcluj.ro>
2024-08-19 21:54:50 +0300

    commit

---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+
+        public int CategoryId { get; set; }
+
+
+        [Column(TypeName = "nvarchar50)")]
+        public string Title {  get; set; }
+
+        [Column(TypeName = "nvarchar5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar50)")]
+        public string Type { get; set; } = "Expense";
+
+    }
+}
---
+++ WebApp/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransationId { get; set; }
+
+        //CategoryId
+
+        public int Amount { get; set; }
+
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
